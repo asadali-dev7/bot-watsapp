7bb5307ace6bdfa38a4f6e88974182a9fc7411f1
asadali-dev7 <mabdulmannan8044@gmail.com>
2025-03-01 13:14:53 +0500

    aad

---
+++ settings.js
@@ -0,0 +1,13 @@
+const settings = {
+  packname: 'Knight',
+  author: 'Bot',
+  botName: "Knight Bot",
+  botOwner: 'H.M.Abdul Mannan', // Your name
+  ownerNumber: '923167476844', //Your number
+  giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
+  commandMode: "public",
+  description: "This is a bot for managing group commands and automating tasks.",
+  version: "1.0.0",
+};
+
+module.exports = settings;
